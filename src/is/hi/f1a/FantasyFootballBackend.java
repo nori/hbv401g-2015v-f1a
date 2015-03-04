b2df9b1d68e02ee4ebe3aae32e8a2bb326a91679
Arnór Valdimarsson <arnor@ja.is>
2015-03-04 17:06:29 +0000

    Skeletons

---
+++ src/is/hi/f1a/FantasyFootballBackend.java
@@ -0,0 +1,28 @@
+package is.hi.f1a;
+
+/**
+ * Created by arnor on 3/4/15.
+ */
+public class FantasyFootballBackend {
+    private League league;
+
+    public FantasyFootballBackend() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public Team getTeam(String team) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private void downloadTeamData() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private void downloadPlayerData() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private void updateRound() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
