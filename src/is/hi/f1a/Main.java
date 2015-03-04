2b0b7a9a553b877d562b488b2de283abf6f4f3d2
tomasnikov <tth25@hi.is>
2015-03-04 16:14:54 +0000

    HOw are you?

--- src/is/hi/f1a/Main.java
+++ src/is/hi/f1a/Main.java
@@ -3,6 +3,6 @@ package is.hi.f1a;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Hello, world. How are you?");
     }
 }
