cd47450a4a79340168cf49f258d31d910d6b218d
arnthorhs <arnthorhs@gmail.com>
2015-03-18 14:24:54 +0000

    bla

--- src/is/hi/f1a/Simulation.java
+++ src/is/hi/f1a/Simulation.java
@@ -1,12 +1,24 @@
 package is.hi.f1a;
 
+import java.util.ArrayList;
+
 public class Simulation {
     private Game game;
+    private Team homeTeam;
+    private Team awayTeam;
+    private ArrayList<Player> home;
+    private ArrayList<Player> away;
 
     public Simulation(Game game) {
         this.game = game;
+        this.awayTeam = this.game.getHomeTeam();
+        this.homeTeam = this.game.getAwayTeam();
+        this.home = homeTeam.calculateStartingTeam();
+        this.away = awayTeam.calculateStartingTeam();
     }
+    public ArrayList<GameEvent> simulate(){
 
+    }
     public void calculateGoals() {
         throw new UnsupportedOperationException("Not, implemented yet");
     }
@@ -15,7 +27,20 @@ public class Simulation {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
-    public void calculateCards() {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public Player calculateCards(ArrayList<Player> team) {
+        //throw new UnsupportedOperationException("Not implemented yet");
+        ArrayList<Player> tempTeam=team;
+        for(Player player:team){
+            if(player.getPosition() == Player.Position.DEFENDER) {
+                tempTeam.add(player);
+            }
+            if (player.getGames()!=0){
+                if (player.getYellowCards()/player.getGames() > 0.5) {
+                    tempTeam.add(player);
+                }
+            }
+        }
+        int rand = ((int)(Math.random()))*tempTeam.size();
+        return tempTeam.get(rand);
     }
 }
